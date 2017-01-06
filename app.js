1372dc951aae5b574096d8d3977e0eeec2dbb68e
alexwander <ryndyuk.alexander@gmail.com>
2017-01-06 19:14:37 +0200

    added routing

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ var categories = require('./routes/categories');
 
 
 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/courses', courses);
+app.use('/categories', categories);
+

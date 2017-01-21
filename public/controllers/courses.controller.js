0127d2bad6cc37dc412faaf89caa5d319d3217df
alexwander <ryndyuk.alexander@gmail.com>
2017-01-21 19:33:11 +0200

    get for courses

--- public/controllers/courses.controller.js
+++ public/controllers/courses.controller.js
@@ -1,1 +1,8 @@
 angular.module("kB")
+
+.controller('CoursesCtrl', ['$scope', '$http', function($scope, $http){
+    $http.get('/courses').success(function(data){
+        $scope.courses = data;
+    });
+}])
+

eaea4f69fdb63754ae7717157e690dfad0e8b8d3
alexwander <ryndyuk.alexander@gmail.com>
2017-02-03 20:45:25 +0200

    update delete course model

--- models/course.js
+++ models/course.js
@@ -63,3 +63,47 @@ module.exports.getCoursesByCategory = function(category, callback){
     var query = {category: category};
     Course.find(query, callback);
 };
+
+// Add an Course
+module.exports.createCourse = function(newCourse, callback){
+    newCourse.save(callback);
+};
+
+// Update Course
+module.exports.updateCourse = function(id, data, callback){
+    var title = data.title;
+    var body = data.body;
+    var category = data.category;
+    var linkurl = data.linkurl;
+    var previmage = data.previmage;
+    var lang = data.lang;
+    var provider = data.provider;
+
+
+
+    var query = {_id: id};
+    Course.findById(id, function(err, course){
+        if(!course){
+            return next(new Error('Coul not load course'));
+        } else {
+            // Update
+            course.title    = title;
+            course.body     = body;
+            course.category = category;
+            course.linkurl = linkurl;
+            course.previmage = previmage;
+            course.provider = provider;
+            course.lang = lang;
+
+
+            course.save(callback);
+        }
+    });
+};
+
+// Remove Course
+module.exports.removeCourse = function(id, callback){
+    Course.find({
+        _id: id
+    }).remove(callback);
+};

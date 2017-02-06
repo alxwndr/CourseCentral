b73d5b0d9da1ae1644672197586b7db35b6667e7
alexwander <ryndyuk.alexander@gmail.com>
2017-02-06 20:49:46 +0200

    details page routing

--- public/app.js
+++ public/app.js
@@ -15,7 +15,26 @@ app.config(['$routeProvider', function($routeProvider){
             templateUrl: 'views/courses.view.html',
             controller: 'CoursesCtrl'
         }).
-      
+    when('/courses/details/:id', {
+        templateUrl: 'views/courses_details.view.html',
+        controller: 'CourseDetailsCtrl'
+    }).
+    when('/courses/category/:category', {
+        templateUrl: 'views/cat_courses.view.html',
+        controller: 'CoursesCategoryCtrl'
+    }).
+    when('/courses/add', {
+        templateUrl: 'views/add_course.view.html',
+        controller: 'CourseCreateCtrl'
+    }).
+    when('/courses/edit/:id', {
+        templateUrl: 'views/edit_course.view.html',
+        controller: 'CourseEditCtrl'
+    }).
+    when('/test', {
+        templateUrl: 'views/test.html',
+        controller: 'CoursesCtrl'
+    }).
 
         otherwise({
             redirectTo: '/'

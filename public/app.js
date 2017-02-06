c6e9816ba8d2c49ab33fb5270eb3b864ae032d86
alexwander <ryndyuk.alexander@gmail.com>
2017-02-06 20:49:24 +0200

    basic routing

--- public/app.js
+++ public/app.js
@@ -1,2 +1,23 @@
 var  app = angular.module('kB', ['ngRoute']);
 
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+        when('/', {
+            templateUrl: 'views/home.html',
+            //////check controller
+            controller: 'CourseEditCtrl'
+        }).
+        when('/categories', {
+            templateUrl: 'views/categories.view.html',
+            controller: 'CategoriesCtrl'
+        }).
+        when('/courses', {
+            templateUrl: 'views/courses.view.html',
+            controller: 'CoursesCtrl'
+        }).
+      
+
+        otherwise({
+            redirectTo: '/'
+        });
+}]);

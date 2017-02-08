3256f80a538f52a51dd92ac1f76891b73ca04e98
alexwander <ryndyuk.alexander@gmail.com>
2017-02-08 20:52:16 +0200

    categories router

--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,22 @@ var router = express.Router();
 
 var Category = require('../models/category');
 
+router.get('/', function(req, res, next) {
+  Category.getCategories(function(err, categories){
+      if(err){
+          console.log(err);
+      }
+      res.json(categories);
+  });
+});
 
+router.get('/:id', function(req, res, next) {
+  Category.getCategoryById(req.params.id, function(err, category){
+      if(err){
+          console.log(err);
+      }
+      res.json(category);
+  });
+});
 
 module.exports = router;
